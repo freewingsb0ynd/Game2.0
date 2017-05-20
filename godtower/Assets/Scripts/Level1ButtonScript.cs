41d6a313733e556521c299548b63db20d82c265e
hoangelato <lhp.huyhoangnguyen@gmail.com>
2017-05-21 00:58:44 +0700

    Level1 UI

---
+++ godtower/Assets/Scripts/Level1ButtonScript.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Level1ButtonScript : MonoBehaviour
+{
+    public void Begin()
+    {
+        TKSceneManager.ChangeScene("Level2");
+    }
+
+
+
+}
---
+++ godtower/Assets/Scripts/StartSceneScript.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class StartSceneScript : MonoBehaviour {
+    public void Begin()
+    {
+        TKSceneManager.ChangeScene("Level1");
+    }
+
+
+
+}
