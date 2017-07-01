fca55071f464c7841761838ef968ed48e7fa9d2a
hoangelato <lhp.huyhoangnguyen@gmail.com>
2017-07-01 15:36:03 +0700

    Rockman Initialized

---
+++ Rockman/Assets/Script/InputController.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InputController : MonoBehaviour {
+    public Vector2 Direction { get; private set; }
+
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	private void Update () {
+        Direction = new Vector2(Input.GetAxis("Horizontal"), 0);
+	}
+}
---
+++ Rockman/Assets/Script/PlayerController.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+    public float movementSpeed;
+
+    private InputController inputController;
+    private Vector2 velocity;
+
+
+    private void Awake()
+    {
+        inputController = GetComponent<InputController>();
+    }
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    private void FixedUpdate()
+    {
+        velocity = new Vector2(inputController.Direction.x * movementSpeed, velocity.y + Physics2D.gravity.y + Time.fixedDeltaTime);
+
+        transform.position += (Vector3)(velocity * Time.fixedDeltaTime);
+    }
+}
